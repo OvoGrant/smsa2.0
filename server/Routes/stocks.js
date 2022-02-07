e99daecac25af710ab204c7e2e569ee9111373b5
Ogheneovo Grant-Oyeye <ogheneovo.grant-oyeye@mail.mcgill.ca>
2022-02-06 21:20:34 -0500

    "restful endpoints created"

---
+++ server/Routes/stocks.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const stocksRouter = express.Router()
+
+stocksRouter.get('/',(req,res)=>{
+
+})
+
+stocksRouter.post('/',(req,res)=>{
+
+})
+
+stocksRouter.get('/:symbol',(req,res)=>{
+
+})
+
+stocksRouter.post('/:symbol',(req,res)=>{
+
+})
+
+stocksRouter.get('/:symbol/time',(req,res) => {
+
+})
+
+stocksRouter.post('/:Symbol/time',(req,res)=>{
+
+})
---
+++ server/Routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const usersRouter = express.Router()
+
+
+
+usersRouter.get('/',(req,res)=>{
+
+})
+
+usersRouter.post('/',(req,res)=>{
+
+})
+
+usersRouter.get('/:id',(req,res)=> {
+
+})
+
+usersRouter.put('/:id',(req,res)=>{
+
+})
+
+usersRouter.delete('/:id',(req,res)=>{
+
+})
+
+usersRouter.get('/:id/holdings',(req,res) => {
+ 
+})
+
+
+
+module.exports = usersRouter
